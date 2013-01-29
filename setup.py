c2027cfd7207067efe17888207e97e67118e608f
Michael van Bracht <michael@wontfix.org>
2013-01-29 19:45:53 +0100

    Update url

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
 
-setup(
+import setuptools as _setuptools
+
+_setuptools.setup(
     name = "klemmbrett",
     version = "0.1-dev",
     description = "Versatile clipboard manager",
     author = "Michael van Bracht",
     author_email = "michael@wontfix.org",
-    url = "http://klemmbrett.wontfix.org",
-    packages = find_packages(),
+    url = "https://github.com/wontfix-org/klemmbrett",
+    packages = _setuptools.find_packages(),
     scripts = ["scripts/klemmbrett"],
     data_files = [
         ("/etc", ["conf/klemmbrett.conf"])
