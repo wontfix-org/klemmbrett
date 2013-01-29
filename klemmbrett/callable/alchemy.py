05897bf3717551832fd733cb7e50770619199a8d
Michael van Bracht <mvb@solute.de>
2013-01-29 10:49:43 +0100

    Interpolate current clipboard contents into statement

--- klemmbrett/callable/alchemy.py
+++ klemmbrett/callable/alchemy.py
@@ -2,4 +2,10 @@
 import sqlalchemy as _sa
 
 def statement(options, plugin):
-    return str(list(_sa.create_engine(options["engine"]).execute(options["statement"]))[0][0])
+    return str(
+        list(
+            _sa.create_engine(options["engine"]).execute(
+                options["statement"] % {'clipboard': plugin.history.top},
+            )
+        )[0][0]
+    )
