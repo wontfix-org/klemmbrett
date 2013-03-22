8d611f4c7006161157235ba8adb4b1b963cabec8
Michael van Bracht <mvb@solute.de>
2013-03-22 13:58:51 +0100

    New callable to autoquote & lineunwrap clipboard contents

--- klemmbrett/callable/__init__.py
+++ klemmbrett/callable/__init__.py
@@ -1,3 +1,7 @@
+import re as _re
 
 def newline_to_comma(options, plugin):
     return plugin.history.top.strip().replace("\n", ",")
+
+def newline_to_comma_quoted(options, plugin):
+    return "'" + "','".join(_re.split(r"\s+", plugin.history.top.strip())) + "'"
